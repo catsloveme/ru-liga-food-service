0f83cb9e228410b529ee39c4af87e32a92da15e5
catsloveme <volkovadeveloper@gmail.com>
2023-10-23 14:30:49 +0300

    add dependence batis starter

--- order-service/src/main/java/ru/liga/configuration/conditional/MyBatisConfiguration.java
+++ order-service/src/main/java/ru/liga/configuration/conditional/MyBatisConfiguration.java
@@ -3,6 +3,7 @@ package ru.liga.configuration.conditional;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import ru.liga.service.OrderItemService;
 import ru.liga.service.OrderService;
 import ru.liga.service.batis.BatisOrderItemService;
@@ -10,10 +11,13 @@ import ru.liga.service.batis.BatisOrderService;
 @Configuration
 @ConditionalOnProperty(prefix = "app", name = "database-access-type", havingValue = "my_batis")
 public class MyBatisConfiguration {
+
+    @Primary
     @Bean("myBatisOrderService")
     public OrderService orderService() {
         return new BatisOrderService();
     }
+    @Primary
     @Bean("myBatisOrderItemService")
     public OrderItemService orderItemService() {
         return new BatisOrderItemService();
