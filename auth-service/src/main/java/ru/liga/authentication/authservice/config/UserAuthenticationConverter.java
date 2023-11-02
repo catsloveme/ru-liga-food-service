5491f4ac03f6973da2894deafa526a7175799290
catsloveme <volkovadeveloper@gmail.com>
2023-11-02 11:26:32 +0300

    authorization on the service Order to read order data

---
+++ auth-service/src/main/java/ru/liga/authentication/authservice/config/UserAuthenticationConverter.java
@@ -0,0 +1,27 @@
+package ru.liga.authentication.authservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.stereotype.Component;
+import ru.liga.authentication.authservice.dto.UserDto;
+
+@Component
+public class UserAuthenticationConverter implements AuthenticationConverter {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    @Override
+    public Authentication convert(HttpServletRequest request) {
+        UserDto userDto = null;
+        try {
+            userDto = MAPPER.readValue(request.getInputStream(), UserDto.class);
+        } catch (IOException e) {
+            return null;
+        }
+        return UsernamePasswordAuthenticationToken.unauthenticated(userDto.getLogin(), userDto.getPassword());
+    }
+}
